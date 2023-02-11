574a590db939ed4a73eae9ac062cd2489236f3c0
Shahid <shahid.alam57@gmail.com>
2023-02-11 23:24:49 +0530

    completed tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Home from './pages/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-   <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div  className='font-Poppins'>
+      <Home/>
     </div>
   )
 }
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <nav>
+        <span>LOGO</span>
+
+        <div>
+        <button>Login</button>
+        <button>Sign Up</button>
+        </div>
+    </nav>
+  )
+}
+
+export default index
