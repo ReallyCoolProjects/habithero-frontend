2131fea2a1e97305f2dc78255676abb309da9e72
Shahid <shahid.alam57@gmail.com>
2023-02-12 00:35:54 +0530

    added new svgs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import DarkButton from '../../components/Buttons/DarkButton'
 import OutlinedButton from '../../components/OutlinedButton';
-import Logo from "../../assets/mindful.svg"
+// import Logo from "../../assets/gym.svg"
+import Logo from "../../assets/SVG/larry-with-friend.svg"
+
 const index = () => {
     const k ='flex flex-col justify-center items-center min-h-screen border w-full border-black';
     const ca= "flex flex-col justify-around items-center h-[50%] w-full"
   return (
     <section className='center-section page'>
         <div className='center-section' id='cta'>
-        <img src={Logo} alt="" className='w-8' />
+        <img src={Logo} alt="" className='w-30' />
         <h1 className='text-5xl font-bold'>HabitHero</h1>
         <p className=''>Designed to help you become the best you</p>
         <div className='center-section horizonatal-button-group'>
