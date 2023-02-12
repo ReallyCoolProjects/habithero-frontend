68dcb1832144c605adc479d8763d61be75fc3f3b
Shahid <shahid.alam57@gmail.com>
2023-02-12 13:49:18 +0530

    added calendar componenet

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
 
 const Dashboard = () => {
+//${i==0? "bg-black" : ""}
+    const days = Array(6).fill(1).map((el, i) =>{
+        return     <div className={`flex flex-col w-10 justify-center`}>
+        <span className='text-center'>Su</span>
+        {/**index can be replaced with present day flag */}
+        <span className={`${i==0? "bg-black text-white" : ""} border-2 border-black h-10 rounded-full flex justify-center items-center`}>23</span>
+</div>
+    }
+  )
+
+
   return (
-    <section>
-        hello
+    <section className='section p-6' id='dashboard'>
+        {/**month selector */}
+    <div className='text-2xl flex justify-between'>
+    <span>January</span>
+    <span>add</span>
+    </div>
+
+    {/**calendar */}
+    <div className='mt-4 flex justify-around'>
+    {days}
+    </div>
     </section>
   )
 }
