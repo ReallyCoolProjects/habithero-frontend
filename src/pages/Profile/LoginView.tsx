068e97d0361dbc352d781d1ca0bf617f4b47e47b
Shahid <shahid.alam57@gmail.com>
2023-02-12 21:29:41 +0530

    added illustration for sign up in profile

--- src/pages/Profile/LoginView.tsx
+++ src/pages/Profile/LoginView.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import SignUpImage from "../../assets/SVG/larry-wait.svg"
 const LoginView = () => {
   return (
     <div className='mt-8'>
-        <div className='flex flex-col h-[40vh] justify-around'>
+        <div className='flex flex-col h-[80vh] justify-around'>
         <span className="text-5xl">Hello, Guest</span>
+        <img src={SignUpImage} alt="" />
         <p> <Link to="/" className='underline'>Sign up</Link> for a free account and track your progress anywhere!</p>
         <p>Already have an account? <Link to={"/"} className='underline'>Login</Link> and get your habits going!</p>
         </div>
