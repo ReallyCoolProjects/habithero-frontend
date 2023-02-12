86d283f3d2071994390e9a0afc20f504aab1d41b
Shahid <shahid.alam57@gmail.com>
2023-02-12 15:13:42 +0530

    added habit table

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,31 +1,67 @@
-import React from 'react'
+import React from "react";
 
 const Dashboard = () => {
-//${i==0? "bg-black" : ""}
-    const days = Array(6).fill(1).map((el, i) =>{
-        return     <div className={`flex flex-col w-10 justify-center`}>
-        <span className='text-center'>Su</span>
-        {/**index can be replaced with present day flag */}
-        <span className={`${i==0? "bg-black text-white" : ""} border-2 border-black h-10 rounded-full flex justify-center items-center`}>23</span>
-</div>
-    }
-  )
+  //${i==0? "bg-black" : ""}
+  const greetingImg =
+    "https://img.freepik.com/free-vector/learning-concept-illustration_114360-6186.jpg?w=740&t=st=1676190711~exp=1676191311~hmac=0c7c6943895dbddc633fb49a270fd4c8b6e9acc47949a564369a06b2a43caa2f";
 
+  const days = Array(6)
+    .fill(1)
+    .map((el, i) => {
+      return (
+        <div className={`flex flex-col w-10 justify-center`}>
+          <span className="text-center">Su</span>
+          {/**index can be replaced with present day flag */}
+          <span
+            className={`${
+              i == 0 ? "bg-black text-white" : ""
+            } border-2 border-black h-10 rounded-full flex justify-center items-center`}
+          >
+            23
+          </span>
+        </div>
+      );
+    });
+
+  const habits = Array(6)
+    .fill(1)
+    .map((el, i) => {
+      return (
+        <tr className="flex capitalize justify-between border-b border-[#b7b8b7] p-2">
+          <span>go for a walk</span>
+          <div className="flex justify-between w-[8rem]">
+            <span>done</span>
+            <span>not done</span>
+          </div>
+        </tr>
+      );
+    });
 
   return (
-    <section className='section p-6' id='dashboard'>
-        {/**month selector */}
-    <div className='text-2xl flex justify-between'>
-    <span>January</span>
-    <span>add</span>
-    </div>
+    <section
+      className="section p-6 grid grid-cols-1 gap-12 grid-rows-6"
+      id="dashboard"
+    >
+      {/**month selector */}
+      <div className="text-2xl flex justify-between">
+        <span>January</span>
+        <span>add</span>
+      </div>
 
-    {/**calendar */}
-    <div className='mt-4 flex justify-around'>
-    {days}
-    </div>
+      {/**calendar */}
+      <div className=" flex justify-around h-16">{days}</div>
+
+      {/**greetings card */}
+      <div className="w-full min-h-8 bg-white flex rounded-2xl p-6 justify-between">
+        <p className="text-2xl flex flex-col items-center justify-center">
+          Good Morning, User!
+        </p>
+        <img src={greetingImg} alt="boy-reading" className="h-28 w-28" />
+      </div>
+
+      <table className="borde">{habits}</table>
     </section>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
